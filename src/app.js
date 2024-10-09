06bdf65e33b9ce83987f70bd16da3044e2b6b9ac
blueclorox <dehydrate92@gmail.com>
2024-10-09 23:29:35 +0900

    feat/project init
    초기 프로젝트 세팅

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import { SERVER_PORT } from './constants/env.constants.js'
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/health-check', (req, res) => {
+    return res.status(200).send(`Healthy`)
+})
+
+app.listen(SERVER_PORT, () => {
+    console.log(`서버가 ${SERVER_PORT}포트에서 실행중입니다.`)
+})
